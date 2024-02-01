8237a0e62f8e664453185098141fdb51ff5f79fc
Luís Eduardo Marinho Fernandes <duduyugi@gmail.com>
2024-02-01 13:21:07 -0300

    initial setup

---
+++ teste-front-end-MOBILE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teste-front-end-MOBILE
+//
+//  Created by Luís Eduardo Marinho Fernandes on 01/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+
+
+}
+
